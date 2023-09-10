f3f901a61154ed00c7ebe28b3b367c39b1db4719
Ilia Choly <ilia.choly@gmail.com>
2023-09-10 18:28:02 -0400

    bundle the plugin

--- init.lua
+++ init.lua
@@ -2,16 +2,6 @@ vim.o.expandtab = false
 vim.o.shiftwidth = 0
 vim.o.softtabstop = -1
 
-vim.cmd.packadd("packer.nvim")
+vim.cmd.packadd("nvim-lspconfig")
 
-require("packer").startup(function(use)
-  use("wbthomason/packer.nvim")
-  use({
-    "neovim/nvim-lspconfig",
-    config = function()
-      require("lspconfig").eslint.setup({})
-    end
-  })
-end)
-
-
+require("lspconfig").eslint.setup({})
