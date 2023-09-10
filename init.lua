fbf7102496a5baa217255aa0f5f8496a81f8f033
Ilia Choly <ilia.choly@gmail.com>
2023-09-10 18:09:43 -0400

    remove vim sleuth

--- init.lua
+++ init.lua
@@ -1,10 +1,11 @@
-vim.o.expandtab = true
+vim.o.expandtab = false
+vim.o.shiftwidth = 0
+vim.o.softtabstop = -1
 
 vim.cmd.packadd("packer.nvim")
 
 require("packer").startup(function(use)
   use("wbthomason/packer.nvim")
-  use("tpope/vim-sleuth")
   use({
     "neovim/nvim-lspconfig",
     config = function()
